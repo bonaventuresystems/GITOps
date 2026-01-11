2f3648c00126b79ef42829a59f62b538962136d6
bonaventuresystems <66865310+bonaventuresystems@users.noreply.github.com>
2026-01-11 16:36:40 +0530

    dd

---
+++ DataProviderAPI/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DataProviderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> data = new List<string>();
+            for (int i = 0; i < 100; i++)
+            {
+                data.Add(new Random().Next().ToString());
+            }
+            return data;
+        }
+    }
+}
