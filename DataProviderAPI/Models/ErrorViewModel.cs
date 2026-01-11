1c80ac831aad314db571cde1222b22ac30b715a0
bonaventuresystems <66865310+bonaventuresystems@users.noreply.github.com>
2026-01-11 16:32:16 +0530

    Check
    Cheeck

---
+++ DataProviderAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataProviderAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
